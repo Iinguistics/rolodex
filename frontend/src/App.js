cfba03c329f76cd74b4866cfd6d3b7bde9f30b5e
James <Thylinguistics@gmail.com>
2021-03-11 14:46:28 -0800

    scaffolding

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,32 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { ToastProvider } from 'react-toast-notifications';
 import Header from './components/Header';
 import Home from './components/Home';
 import Create from './components/Create';
 import Product from './components/Product';
 
+
 function App() {
+
+
+
+
+  
   return (
     <Router>
       <Header />
-      <div className="container">
-      <Route path="/" exact component={Home} />
-      <Route path="/create" exact component={Create} />
-      <Route path="/product/:id"  component={Product} />
-      </div>
+      <main className="py-5">
+      <Container>
+      <ToastProvider autoDismiss={true} autoDismissTimeout={3500} placement='bottom-center' >
+        <Route path="/" exact component={Home} />
+        <Route path="/create" exact component={Create} />
+        <Route path="/product/:id"  component={Product} />
+        </ToastProvider>
+      </Container>
+      </main>
+     
+      
     </Router>
   );
 }
