28639c961ccf1ed9baeaf9f12caec0dbfd4315a2
James <Thylinguistics@gmail.com>
2021-03-19 18:55:15 -0700

    ...

---
+++ backend/models/viewersSnapshotModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const viewerSnapshotSchema = mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    chatter_count:{
+       type: Number,
+       required: true
+    },
+    vips: [String],
+    moderators: [String],
+    staff: [String],
+    admins: [String],
+    global_mods: [String],
+    viewers: [String],
+
+ 
+}, {
+    timestamps: true
+});
+
+const viewerSnapshot = mongoose.model('viewerSnapshot', viewerSnapshotSchema);
+module.exports = viewerSnapshot;
