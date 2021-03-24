8afe0808a0f256449b04f8ec6e4b628e85f2127c
James <Thylinguistics@gmail.com>
2021-03-23 18:46:24 -0700

    ...

--- backend/controllers/viewersSnapshotController.js
+++ backend/controllers/viewersSnapshotController.js
@@ -2,6 +2,19 @@ const asyncHandler = require('express-async-handler');
 const ViewerSnapshot = require('../models/viewersSnapshotModel');
 const axios = require('axios');
 
+
+
+// Fetch logged in users viewers    
+//@route  GET /api/viewers/sanpshot
+const getSnapshots = asyncHandler(async(req,res)=>{
+
+    const snapshots = await ViewerSnapshot.find({ user: req.user._id });
+    res.json({ snapshots });
+
+});
+
+
+
 // Create new snapshot 
 //@route  POST api/viewers/snapshot
 //@access Have to be logged in
@@ -33,4 +46,4 @@ const createSnapshot = asyncHandler(async(req,res)=>{
 
 
 
-module.exports = { createSnapshot }
+module.exports = { createSnapshot, getSnapshots }
