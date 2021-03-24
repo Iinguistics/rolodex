d26c5ee986a9b26d3336d417a66e66f13a1a3e36
James <Thylinguistics@gmail.com>
2021-03-24 00:12:01 -0700

    ...

--- frontend/src/components/CaptureChart.js
+++ frontend/src/components/CaptureChart.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Scatter, Bar } from 'react-chartjs-2';
 import Message from './bootstrapHelpers/Message';
+import { setRandomFallback } from 'bcryptjs';
 
 
 const CaptureChart = ({ userInfo, history }) => {
     const [captureData, setCaptureData] = useState([]);
     const [captureDataError, setCaptureDataError] = useState("");
+
+
 
 
 
@@ -20,7 +24,9 @@ const CaptureChart = ({ userInfo, history }) => {
                 }
             }
             const { data } = await axios.get('/api/snapshot', config);
-            setCaptureData(data);
+            setCaptureData(data.snapshots);
+
+             
            
         }catch (error){
           setCaptureDataError(error.message)
@@ -35,15 +41,78 @@ const CaptureChart = ({ userInfo, history }) => {
 
         fetchCaptures();
 
-    }, [])
+
+    }, []);
+
+   
+
+    
+    console.log(captureData)
+
+    const rand = () => Math.round(Math.random() * 20 - 10)
+
+    const data = {
+    datasets: [
+        {
+        label: 'Viewers',
+        data: [
+            { x: rand(), y: rand() },
+            { x: rand(), y: rand() },
+            { x: rand(), y: rand() },
+            { x: rand(), y: rand() },
+            { x: rand(), y: rand() },
+            { x: rand(), y: rand() },
+            { x: rand(), y: rand() },
+            { x: rand(), y: rand() },
+            { x: rand(), y: rand() },
+            { x: rand(), y: rand() },
+            { x: rand(), y: rand() },
+            { x: rand(), y: rand() },
+            { x: rand(), y: rand() },
+            { x: rand(), y: rand() },
+        ],
+        backgroundColor: 'rgba(255, 99, 132, 1)',
+        },
+    ],
+    }
+
+    const options = {
+    scales: {
+        yAxes: [{
+            ticks: {
+            beginAtZero: true,
+            },
+            scaleLabel: {
+                display: true,
+                labelString: 'Viewer Count'
+              }
+        }],
+        xAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: 'Dates'
+            }
+          }],
+    },
+    }
+    
 
 
-   console.log(captureData)
+       
+
+
 
     return (
         <div>
             {captureDataError && <Message variant="danger">{captureDataError}</Message> }
             testing
+
+            <Scatter
+                data={data}
+                width={100}
+                height={50}
+                options={options}
+            />
         </div>
     )
 }
