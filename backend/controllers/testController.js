c865c586bb238901d42f91605fcbaa79b26ad196
James <Thylinguistics@gmail.com>
2021-03-25 19:11:10 -0700

    ...

--- backend/controllers/testController.js
+++ backend/controllers/testController.js
@@ -4,18 +4,13 @@ const axios = require('axios');
 
 
 // Create new snapshot 
-//@route  POST api/viewers/snapshot
+//@route  POST api/snapshot
 //@access Have to be logged in
 const test = asyncHandler(async(req,res)=>{
     const { token, name } = req.body;
 
     //const testChannel = 'contv'
     
-     
-        //get token
-       //const tokenData = await axios.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&grant_type=client_credentials`);
-       //tokenData.data.access_token
-
         try{
 
         const config = {
@@ -25,7 +20,6 @@ const test = asyncHandler(async(req,res)=>{
                  'Client-ID': `${process.env.TWITCH_CLIENT_ID}`
             }
         }
-        //const twitchToken = tokenData.data.access_token;
 
         // get specified users info, including their user_id & viewer_count
         const { data } = await axios.get(`https://api.twitch.tv/helix/streams?user_login=${name}`, config);
@@ -34,9 +28,7 @@ const test = asyncHandler(async(req,res)=>{
         console.log(data);
         
          }catch(error){
-            
             res.status(404).json(error.message);
-            
         }
 });
 
@@ -55,9 +47,7 @@ const fetchToken = asyncHandler(async(req,res)=>{
          res.status(200).json(twitchToken);
          
           }catch(error){
-             
              res.status(404).json(error.message);
-             
          }
  });
 
