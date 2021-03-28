49f7f1cb00463fe0fc92c9df6eb86096913749fa
James <Thylinguistics@gmail.com>
2021-03-28 11:42:16 -0700

    ...

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -21,8 +21,8 @@ const HomeScreen = ({ userInfo }) => {
              <Jumbotron className="my-5 main-jumbo-bg shadow">
                <h1 className="font-weight-bold home-title my-4">Get to know your viewers & grow your channel</h1>
                <h3 id="home-title-sub" className="d-inline font-weight-bold">Data tool</h3><br /> <h3>for streamers who <br />are dedicated to their craft</h3>
-               {userInfo ? <Link to="/profile" className="btn btn-light mt-3">My profile</Link> :
-               <Link to="/login" className="btn btn-light mt-3">Log in</Link>
+               {userInfo ? <Link to="/profile" className="btn btn-primary mt-3 shadow">My profile</Link> :
+               <Link to="/login" className="btn btn-light mt-3 shadow">Log in</Link>
                }
             </Jumbotron>
             </section>
